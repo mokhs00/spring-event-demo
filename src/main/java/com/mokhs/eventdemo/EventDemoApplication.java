1160b40ad24c8ca2b689d641829ff235067ea180
mokhs00 <mokhs00@naver.com>
2021-10-16 21:17:21 +0900

    init : project init

---
+++ src/main/java/com/mokhs/eventdemo/EventDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mokhs.eventdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDemoApplication.class, args);
+    }
+
+}
